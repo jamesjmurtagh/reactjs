f1fc827c3902ec95b97e22049fa2791e296b753e
james <james@linux.gateway>
2017-12-20 09:29:48 +1000

    Initial setup includes a default redux state with a placeholder reducer.

--- ui/src/app/index.js
+++ ui/src/app/index.js
@@ -1,1 +1,28 @@
-console.log("hello world!");
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, combineReducers} from 'redux'
+import {Provider, connect} from 'react-redux'
+
+import {testReducer} from './redux/reducers'
+
+// Reducers ...
+const reducer = combineReducers({
+        test: testReducer
+})
+
+// Initial state ...
+var initialState = {
+        test: null
+}
+
+// Store ...
+let store = createStore(reducer, initialState)
+store.subscribe(() => {
+        saveState(store.getState());
+        console.log(store.getState());
+})
+
+// Render ...
+ReactDOM.render(
+        <h1>Hello, world!</h1>, document.getElementById('app')
+)
